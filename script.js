dad1ef9179f99a38d4acf1ce4fde1d0562639d36
AngelicaCahum2525 <monsecahuum@gmail.com>
2024-02-24 01:36:05 -0600

    Nav creado

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let cssLink = document.createElement("link");
 cssLink.rel = "stylesheet";
-cssLink.href = "CSS/style.css"
+cssLink.href = "CSS/style.css";
 
+document.head.appendChild(cssLink);
 
-//Se establece el contenedor principal en una variable constante 
+// Se establece el contenedor principal en una variable constante 
 const appContainer = document.getElementById("app");
 
 // Se crea la variable nav_container donde tendra un <div>
@@ -11,9 +12,10 @@ var nav_container = document.createElement("div");
 //El establece que el id del div sera nav
 nav_container.setAttribute("id", "nav");
 
+//Agrega elementos al contenedor principal
+appContainer.appendChild(nav_container);
+
 //Accedo a div por medio del id
 let nav = document.getElementById("nav");
 nav.innerHTML = "Hola, aqui va el logo de duoling";
 
-//Agrega elementos al contenedor principal
-appContainer.appendChild(nav_container);
